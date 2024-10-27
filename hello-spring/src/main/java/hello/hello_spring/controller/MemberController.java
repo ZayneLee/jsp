2b06e4e09cb6cd8c68fac5a6e0deb1ef69f4e230
Zayne <star1582@naver.com>
2024-10-27 21:24:59 +0900

    자바코드로 스프링 빈 등록

--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -8,7 +8,7 @@ import hello.hello_spring.service.MemberService;
 @Controller
 public class MemberController {
 
-	private MemberService memberService;
+	private final MemberService memberService;
 	
 	@Autowired
 	public MemberController(MemberService memberService) {
--- hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -6,17 +6,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
-
 import hello.hello_spring.domain.Member;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
 	private static Map<Long, Member> store = new HashMap<Long, Member>();
 	private static long sequence = 0L;
 
-	@Override
 	public Member save(Member member) {
 		member.setId(++sequence);
 		store.put(member.getId(), member);
