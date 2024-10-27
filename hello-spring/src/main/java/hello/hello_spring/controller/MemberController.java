ff5c821cc74572ab42395f45895427e2ea2011c8
Zayne <star1582@naver.com>
2024-10-27 21:18:08 +0900

    Autowierd 추가

--- hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,5 +1,6 @@
 package hello.hello_spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import hello.hello_spring.service.MemberService;
@@ -8,7 +9,8 @@ import hello.hello_spring.service.MemberService;
 public class MemberController {
 
 	private MemberService memberService;
-
+	
+	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
 	}
--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hello_spring.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import hello.hello_spring.domain.Member;
@@ -13,7 +14,8 @@ import hello.hello_spring.repository.MemoryMemberRepository;
 public class MemberService {
 
 	private MemberRepository memberRepository;
-
+	
+	@Autowired
 	public MemberService(MemberRepository memberRepository) {
 		this.memberRepository = memberRepository;
 	}
