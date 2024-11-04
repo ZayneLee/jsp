a641f213bc7144de8ee751a53ed652b410962d42
Zayne <star1582@naver.com>
2024-11-04 20:31:39 +0900

    시간측정 로직 개발

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -15,12 +15,20 @@ public class MemberService {
 	}
 
 	public Long join(Member member) {
+		
+		long start = System.currentTimeMillis();
+		
+		try {
+			// 중복 이름 회원 가입 X
+			validateDuplicateMember(member);
+			memberRepository.save(member);
+			return member.getId();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("timeMs = " + timeMs + "ms"); 
+		}
 
-		// 중복 이름 회원 가입 X
-		validateDuplicateMember(member);
-
-		memberRepository.save(member);
-		return member.getId();
 	}
 
 	private void validateDuplicateMember(Member member) {
