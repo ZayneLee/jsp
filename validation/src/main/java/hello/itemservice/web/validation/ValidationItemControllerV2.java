88c0a4a1ce9408b5aed9ab1e987aa901fe538f68
Zayne <star1582@naver.com>
2024-11-13 21:27:45 +0900

    v2 세팅

---
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -0,0 +1,99 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/validation/v2/items")
+@RequiredArgsConstructor
+public class ValidationItemControllerV2 {
+
+	private final ItemRepository itemRepository;
+
+	@Autowired
+	public ValidationItemControllerV2(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+
+	@GetMapping
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+		return "validation/v2/items";
+	}
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable("itemId") long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v2/item";
+	}
+
+	@GetMapping("/add")
+	public String addForm(Model model) {
+		model.addAttribute("item", new Item());
+		return "validation/v2/addForm";
+	}
+
+	@PostMapping("/add")
+	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+		Map<String, String> errors = new HashMap<>();
+
+		if (!StringUtils.hasText(item.getItemName())) {
+			errors.put("itemName", "ItemName Required");
+		}
+
+		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+			errors.put("price", "price should be from 1000 to 1000000");
+		}
+
+		if (item.getQuantity() == null || item.getQuantity() > 9999) {
+			errors.put("quantity", "Quantity should be upto 9999");
+		}
+
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			int resultPrice = item.getPrice() * item.getQuantity();
+			if (resultPrice < 10000) {
+				errors.put("globalError",
+						"price * quantity should be more than 10,000. current price is " + resultPrice);
+			}
+		}
+
+		if (!errors.isEmpty()) {
+			model.addAttribute("errors", errors);
+			return "validation/v2/addForm";
+		}
+
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable("itemId") Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v2/editForm";
+	}
+
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable("itemId") Long itemId, @ModelAttribute Item item) {
+		itemRepository.update(itemId, item);
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+}
