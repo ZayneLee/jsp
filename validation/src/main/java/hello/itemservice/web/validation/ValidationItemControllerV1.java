538cbf270bae49b05910778ed2645570e31e6cd9
Zayne <star1582@naver.com>
2024-11-13 21:20:38 +0900

    오류 수정

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -7,93 +7,93 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
 public class ValidationItemControllerV1 {
 
-	
-    private final ItemRepository itemRepository;
+	private final ItemRepository itemRepository;
 
-    @Autowired
-    public ValidationItemControllerV1(ItemRepository itemRepository) {
+	@Autowired
+	public ValidationItemControllerV1(ItemRepository itemRepository) {
 		this.itemRepository = itemRepository;
 	}
 
 	@GetMapping
-    public String items(Model model) {
-        List<Item> items = itemRepository.findAll();
-        model.addAttribute("items", items);
-        return "validation/v1/items";
-    }
-
-    @GetMapping("/{itemId}")
-    public String item(@PathVariable("itemId") long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "validation/v1/item";
-    }
-
-    @GetMapping("/add")
-    public String addForm(Model model) {
-        model.addAttribute("item", new Item());
-        return "validation/v1/addForm";
-    }
-
-    @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
-
-	Map<String, String> errors = new HashMap<>();
-	
-	if (!StringUtils.hasText(item.getItemName())) {
-		errors.put("itemName","ItemName Required");
+	public String items(Model model) {
+		List<Item> items = itemRepository.findAll();
+		model.addAttribute("items", items);
+		return "validation/v1/items";
 	}
-	
-	if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-		errors.put("price","price should be from 1000 to 1000000");
+
+	@GetMapping("/{itemId}")
+	public String item(@PathVariable("itemId") long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v1/item";
 	}
-	
-	if (item.getQuantity() == null || item.getQuantity() > 9999) {
-		errors.put("quantity","Quantity should be upto 9999");
-	}
-	
-	if (item.getPrice() != null && item.getQuantity() != null) {
-		int resultPrice = item.getPrice() * item.getQuantity();
-		if (resultPrice < 10000)
-		{
-			errors.put("globalError","price * quantity should be more than 10,000. current price is " + resultPrice);
-		}
-	}
-	
-	if (!errors.isEmpty())
-	{
-		model.addAttribute("errors", errors);
+
+	@GetMapping("/add")
+	public String addForm(Model model) {
+		model.addAttribute("item", new Item());
 		return "validation/v1/addForm";
 	}
-	    
-        Item savedItem = itemRepository.save(item);
-        redirectAttributes.addAttribute("itemId", savedItem.getId());
-        redirectAttributes.addAttribute("status", true);
-        return "redirect:/validation/v1/items/{itemId}";
-    }
 
-    @GetMapping("/{itemId}/edit")
-    public String editForm(@PathVariable("itemId") Long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "validation/v1/editForm";
-    }
+	@PostMapping("/add")
+	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
 
-    @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable("itemId") Long itemId, @ModelAttribute Item item) {
-        itemRepository.update(itemId, item);
-        return "redirect:/validation/v1/items/{itemId}";
-    }
+		Map<String, String> errors = new HashMap<>();
+
+		if (!StringUtils.hasText(item.getItemName())) {
+			errors.put("itemName", "ItemName Required");
+		}
+
+		if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+			errors.put("price", "price should be from 1000 to 1000000");
+		}
+
+		if (item.getQuantity() == null || item.getQuantity() > 9999) {
+			errors.put("quantity", "Quantity should be upto 9999");
+		}
+
+		if (item.getPrice() != null && item.getQuantity() != null) {
+			int resultPrice = item.getPrice() * item.getQuantity();
+			if (resultPrice < 10000) {
+				errors.put("globalError",
+						"price * quantity should be more than 10,000. current price is " + resultPrice);
+			}
+		}
+
+		if (!errors.isEmpty()) {
+			model.addAttribute("errors", errors);
+			return "validation/v1/addForm";
+		}
+
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/validation/v1/items/{itemId}";
+	}
+
+	@GetMapping("/{itemId}/edit")
+	public String editForm(@PathVariable("itemId") Long itemId, Model model) {
+		Item item = itemRepository.findById(itemId);
+		model.addAttribute("item", item);
+		return "validation/v1/editForm";
+	}
+
+	@PostMapping("/{itemId}/edit")
+	public String edit(@PathVariable("itemId") Long itemId, @ModelAttribute Item item) {
+		itemRepository.update(itemId, item);
+		return "redirect:/validation/v1/items/{itemId}";
+	}
 
 }
-
